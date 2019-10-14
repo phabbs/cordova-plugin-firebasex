4544338a72e91f5f5b0fb53f83b55bfb29036cbf
phabbs <abdulmumeen.olasode@andela.com>
2019-10-14 01:12:25 +0100

    Remove changes

--- src/ios/FirebasePlugin.h
+++ src/ios/FirebasePlugin.h
@@ -1,24 +1,22 @@
 #import <Cordova/CDV.h>
 
 @interface FirebasePlugin : CDVPlugin
-+ (FirebasePlugin *) firebasePlugin;
++ (FirebasePlugin *)firebasePlugin;
 
-- (void)startTrace:(CDVInvokedUrlCommand*)command;
-- (void)incrementCounter:(CDVInvokedUrlCommand*)command;
-- (void)stopTrace:(CDVInvokedUrlCommand*)command;
-- (void)setPerformanceCollectionEnabled:(CDVInvokedUrlCommand*)command;
+- (void)startTrace:(CDVInvokedUrlCommand *)command;
+- (void)incrementCounter:(CDVInvokedUrlCommand *)command;
+- (void)stopTrace:(CDVInvokedUrlCommand *)command;
+- (void)setPerformanceCollectionEnabled:(CDVInvokedUrlCommand *)command;
 
-- (void) handlePluginExceptionWithContext: (NSException*) exception :(CDVInvokedUrlCommand*)command;
-- (void) handlePluginExceptionWithoutContext: (NSException*) exception;
-- (void)_logError: (NSString*)msg;
+- (void)handlePluginExceptionWithContext:(NSException *)exception
+                                        :(CDVInvokedUrlCommand *)command;
+- (void)handlePluginExceptionWithoutContext:(NSException *)exception;
+- (void)_logError:(NSString *)msg;
 
 - (void)createChannel:(CDVInvokedUrlCommand *)command;
 - (void)setDefaultChannel:(CDVInvokedUrlCommand *)command;
 - (void)deleteChannel:(CDVInvokedUrlCommand *)command;
 - (void)listChannels:(CDVInvokedUrlCommand *)command;
-@property (nonatomic, copy) NSString *notificationCallbackId;
-@property (nonatomic, copy) NSString *tokenRefreshCallbackId;
-@property (nonatomic, retain) NSMutableArray *notificationStack;
-@property (nonatomic, readwrite) NSMutableDictionary* traces;
+@property(nonatomic, readwrite) NSMutableDictionary *traces;
 
 @end
